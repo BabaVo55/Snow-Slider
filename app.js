c252000442bbc513fc6fc034dc543b4ff8c6547e
BabaVo55 <ethemcode@outlook.com>
2025-01-14 16:59:06 +0000

    javsa partially done

--- app.js
+++ app.js
@@ -11,8 +11,26 @@ function updateSlideTransforms(){
         slide.classList.remove("active");
 
         if (diff < 0) {
-            slide.style.transform = `rotate${diff * 5}deg`;
-            translateY($)
+            slide.style.transform = `rotate(${diff * 5}deg)
+            translateY(${Math.abs(diff) * 50}px)
+            translateX(${diff * 100}%)`;
+            slide.style.zIndex = diff;
+        } else if ( diff > 0) {
+            slide.style.transform = `rotate(${diff * 5}deg)
+            translateY(${Math.abs(diff) * 50}px)
+            translateX(${diff * 100}%)`;
+            slide.style.zIndex = -diff;
+        } else {
+            slide.style.transform  = "rotate(0deg) translateY(0) translateX(0)";
+            slide.style.zIndex = 0;
+            slide.classList.add("active")
         }
     })
+
+    prevButton.disabled = activeIndex === 0;
+    nextButton.disabled = activeIndex === slides.length - 1;
+    prevButton.style.opacity = prevButton.disabled ? "0.1" : "1"
+    nextButton.style.opacity = nextButton.disabled ? "0.1" : "1"
 }
+
+updateSlideTransforms()
